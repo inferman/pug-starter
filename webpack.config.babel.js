45bf3794e8206939e041e445e2f860a4fe8056a8
inferman <infernal.manowar@gmail.com>
2019-09-02 13:56:33 +0300

    configured scss

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,46 +1,68 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 export default {
-  entry: path.join(__dirname, 'src/index.js'),
+  entry: path.join(__dirname, "src/index.js"),
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].bundle.js',
+    path: path.join(__dirname, "dist"),
+    filename: "[name].bundle.js"
   },
   module: {
     rules: [
       {
         test: /\.js/,
         exclude: /(node_modules)/,
-        use: ['babel-loader', 'eslint-loader'],
+        use: ["babel-loader", "eslint-loader"]
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader'],
+      // },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
+        use: ["file-loader"]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
+        use: ["file-loader"]
       },
-    ],
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'index.html'),
-      favicon: 'assets/img/favicon.png',
+      template: path.join(__dirname, "index.html"),
+      favicon: "assets/img/favicon.png"
     }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    })
   ],
-  stats: 'minimal',
-  devtool: 'source-map',
-  mode: 'development',
+  stats: "minimal",
+  devtool: "source-map",
+  mode: "development",
   devServer: {
     open: false,
-    contentBase: './dist',
+    contentBase: "./dist",
     inline: true,
-    port: 4000,
-  },
+    port: 4000
+  }
 };
